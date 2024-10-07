27723b7990c1234ac8947c33cf6871a5a6fa3567
Oisin McCrossan <oisinmccrossan@gmail.com>
2024-10-07 14:44:27 +0000

    Initial commit

---
+++ Components/create.js
@@ -0,0 +1,14 @@
+<footer></footer> // This is an HTML footer element, currently empty
+
+// Define the create component
+const create = () => {
+    return (
+        <div>
+            {/* Display a heading with the text "Create" */}
+            <h1>Create</h1>
+        </div>
+    );
+}
+
+// Export the create component as the default export
+export default create;
--- Components/footer.js
+++ Components/footer.js
@@ -1,5 +1,8 @@
+// Define the Footer component
 const Footer = () => {
-    return <h1>My Footer in another component</h1>;
-  };
-  
-  export default Footer;
+  // Return a heading element with the text "My Footer in another component"
+  return <h1>My Footer in another component</h1>;
+};
+
+// Export the Footer component as the default export
+export default Footer;
